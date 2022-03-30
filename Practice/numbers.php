bc60e94f43bf0b15a3ec7a34159f263d3aa8f7ca
Jay <jayjogi444@gmail.com>
2022-03-30 15:21:26 +0530

    Completed till Number

---
+++ Practice/numbers.php
@@ -0,0 +1,67 @@
+<?php
+
+
+$x = 5985;
+print "Here we will check the number is $x or not Integer: ";
+var_dump(is_int($x));
+echo"<br>";
+
+$x = 59.85;
+print "Here we will check the number is $x is or not Integer: ";
+var_dump(is_int($x));
+echo"<br>";
+
+$x = 10.365;
+print "Here we will check the number is $x is or not Float: ";
+echo "".is_float($x);
+echo"<br>";
+
+$x = 1.9e411;
+print "Here the given Value of x is Finite or infinite: $x  ";
+echo"<br>";
+
+$x = acos(8);
+print "Here the given value of x is or not a number: $x  ";
+echo"<br>";
+
+$x = 5985;
+print "Here we will check the Value is $x is or not numeric: ";
+var_dump(is_numeric($x));
+echo"<br>";
+
+$x = "5985";
+print "Here we will check the value is $x is or not numeric: ";
+var_dump(is_numeric($x));
+echo"<br>";
+
+$x = "59.85" + 100;
+print "Here we will check the value is $x is or not numeric: ";
+var_dump(is_numeric($x));
+echo"<br>";
+
+$x = "Hello";
+print "Here we will check the value is $x is or not numeric: ";
+var_dump(is_numeric($x));
+echo"<br>";
+
+// Cast float to int
+$x = 23465.768;
+echo "Here the given value of x is $x which is Float";
+echo"<br>";
+echo "Here is the value of x after casting it to interger: ".(int)$x;
+
+echo "<br>";
+
+// Cast string to int
+$x = "23465.768";
+echo "Here the given value of x is $x which is string";
+
+echo "Here is the value of x after casting it to interger: ".(int)$x;
+
+echo "<br>";
+
+echo "Here is the value of ".(int)$x." after casting it to Float: ".(float)$x;
+
+?> 
+
+
---
+++ Practice/strings.php
@@ -0,0 +1,24 @@
+<?php
+$str ="Hello World";
+
+echo "Orginal string is $str";
+echo "<br>";
+echo "Length of a string is ".strlen($str);
+echo "<br>";
+echo "String has ".str_word_count($str)." Words";
+echo "<br>";
+echo "Reverse of the orignal string: ".strrev($str);
+echo "<br>";
+echo "Position of the World in orginal String is ".strpos($str,"World");
+echo "<br>";
+echo "I replaced World with Everone from the given string and the new string is ".str_replace("World","Everyone",$str);
+echo "<br>";
+echo "String converted to lowercase ".strtolower($str);
+echo "<br>";
+echo "String converted to uppercase ".strtoupper($str);
+echo "<br>";
+
+
+
+
+?>
