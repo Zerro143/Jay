27568536b97fe78b429e76d0252f616e9e7e6682
Jay <jayjogi444@gmail.com>
2022-03-30 16:31:57 +0530

    Constant done

---
+++ Practice/constant.php
@@ -0,0 +1,14 @@
+<?php
+
+define("lan", [
+    "1"=>"Suprabhat, aapke ane pr khushi ka mahool cha gaya", 
+    "2"=>"Suprabhat Tame aya amne khub aanad ayo ",
+    "3"=>"Welcome, It our Pleasure to have u here"
+  ]);
+
+
+$a=rand(1,3);
+
+echo lan[$a]; echo"<br>";
+
+?> 
