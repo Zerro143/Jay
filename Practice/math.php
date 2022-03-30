3d996f1a1976540979cad8ff34f2c0e21ae0f859
Jay <jayjogi444@gmail.com>
2022-03-30 15:49:27 +0530

    Maths function in php done

--- Practice/math.php
+++ Practice/math.php
@@ -2,9 +2,22 @@
 echo "<h1> Maths</h1>";
 
 echo "pi() will print the value of ".pi();
-echo "<br>"
+echo "<br>";
+
+$a = 10.5; $b = -12; $c = 4;
+echo"min() will print the min value from ($a,$b,$c): ".min($a,$b,$c);  
+echo "<br>";
+echo"max() will print the max value from ($a,$b,$c): ".max($a,$b,$c);  
+echo "<br>";
+echo"abs($b) will print the absolute positive of given value: ".abs($b);  
+echo "<br>";
+echo"sqrt($c) will print the square root of the given value: ".sqrt($c);  
+echo "<br>";
+echo"round($a) will print the nearest interger of the given value: ".round($a); 
+echo "<br>"; 
+echo"rand() will print the random number: ".rand(); 
+echo "<br>"; 
+echo"rand($c,$a) will print the random number between $c and $a: ".rand($c,$a); 
 
 
-
-
-?>
+?>  
