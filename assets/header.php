9b1268e8c73aff16e078132bd33e07ba4d9a60e9
Jay <jayjogi444@gmail.com>
2022-03-30 10:26:28 +0530

    just practice

---
+++ assets/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+<link rel="stylesheet" href="/Jay/assets/main.css">
+<?php
+function active($currect_page){
+  $url_array =  explode('/', $_SERVER['REQUEST_URI']) ;
+  $url = end($url_array);  
+  if($currect_page == $url){
+      echo 'active'; //class name in css 
+  } 
+}
+?>
+<div class="header">
+  <a href="http://localhost/Jay/index.php" class="logo">Game Mania</a>
+  <div class="header-right">
+    <a class="<?php active('index.php');?>"href="http://localhost/Jay/index.php">Home</a>
+    <a class="<?php active('contact.php');?>"href="#contact">Contact</a>
+    <a class="<?php active('about.php');?>"href="#about">About</a>
+    <a class="<?php active('#Login');?>"href="#login" class="open-button" onclick="openForm()">Login</a>
+    <a class="<?php active('regform.php');?>"href="http://localhost/Jay/assets/regform.php">Registration</a>
+  </div>
+</div>
+
+<div style="padding-left:20px">
+
+<div class="form-popup" id="myForm">
+  <form action="#" class="form-container">
+    <h2>Login</h2>
+    <hr>
+    <div class="row">
+      
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
+    </div>
+    <div class="row"  
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+    </div>    
+    <button type="submit" class="btn">Login</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </form>
+</div>
+<section id="Login">
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
+</section>
+
+  
+
+</body>
+</html>
---
+++ assets/regform.php
@@ -0,0 +1,206 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<head>
+    <title>Registration form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+
+
+<?php include 'header.php';?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/Jay/assets/main.css">
+
+
+<script>
+/*function validate() {    
+            var fname = document.regform.fname;    
+            var lname = document.regform.lname;    
+            var mail = document.regform.mail;    
+            
+            if (fname.value.length <= 0) {    
+                alert("Please Enter Your Name");    
+                fname.focus();    
+                return false;    
+            }    
+            if (lname.value.length <= 0) {    
+                alert("Please Enter Your Last Name");    
+                lname.focus();    
+                return false;    
+            }    
+                 
+            if (mail.value.length <= 0) {    
+                alert("Please Enter Your Email");    
+                mail.focus();    
+                return false;    
+            }    
+             
+            if (psw.value.length <= 0) {    
+                alert("Password is required");    
+                course.focus();    
+                return false;    
+            }    
+            if (psw_r.value !== psw.value) {    
+                alert("Password Does not Match");    
+                course.focus();    
+                return false;    
+            } 
+
+            return false;    
+        }    */
+</script>
+
+<?php
+$fnameErr = $lnameErr = $genderErr = $mailErr  = $pswErr = $psw_repeatErr = "";
+$fname = $lname = $gender =$mail  = $psw = $psw_repeat = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  if (empty($_POST["fname"])) {
+    $fnameErr = "Please Enter Your First Name";
+    
+  } else {
+    $fname = test_input($_POST["fname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$fname)) {
+      $fnameErr = "Only letters and white space allowed";
+      
+    }
+  }
+
+  if (empty($_POST["lname"])) {
+    $lnameErr = "Please Enter Your Last Name";
+    
+  } else {
+    $lname = test_input($_POST["lname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$lname)) {
+      $lnameErr = "Only letters and white space allowed";
+      
+    }
+  }
+
+  /*if (empty($_POST["gender"])) {
+    $genderErr = "Please Select Your Gender";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }*/
+
+  if (empty($_POST["mail"])) {
+    $mailErr = "Email is required";
+    
+  } else {
+    $mail = test_input($_POST["mail"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+      $mailErr = "Please enter a valid Email Address";
+      
+    }
+  }
+   
+ /*if (empty($_POST["psw"])) {
+   $pswErr= "Please Enter Your Password";
+   
+  } else {
+    $psw = test_input($_POST["psw"]);
+   // check if URL address syntax is valid
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$psw)) {
+        $pswErr = "Please Enter a Valid Password";
+   }    
+  }*/
+  if (empty($_POST["psw_repeat"])) {
+    $psw_repeatErr = "Please Enter Your Password Again";
+    
+  } else {
+        $psw_repeat= test_input($_POST["psw_repeat"]);
+        // check if URL address syntax is valid
+        if ($psw_repeat!=$psw) {
+        $psw_repeatErr = "Password Does Not Match";
+        
+        }
+    } 
+}  
+    
+?>
+<div class="container">
+  <form name="regform"  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"  enctype="multipart/form-data">
+    <div class="row">
+        <h2><i class="fa fa-user icon"></i> Registration Form </h2>
+        <hr> 
+    </div>
+    <div class="row">
+    
+      <div class="col-25">
+        <label for="fname">First Name</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="fname" name="fname" placeholder="Your name.."required>
+        <span class="error">* <?php echo $fnameErr;?></span>
+      </div>
+   
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label for="lname">Last Name</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="lname" name="lname" placeholder="Your last name.." required>
+        <span class="error">* <?php echo $lnameErr;?></span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label >Gender:</label>
+      </div>
+      <div class="col-75">
+        <input type="radio"  id="male" name="gender" value="male">
+        <label for="male">Male</label>
+        <input type="radio" id="female" name="gender" value="female">
+        <label for="female">Female</label><br>
+        
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+      <label for="mail">Email:</label>
+      </div>
+      <div class="col-75">
+      <input type="email" id="mail" name="mail"placeholder="Email" required>  
+      <span class="error">* <?php echo $mailErr;?></span>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col-25">
+            <label for="psw">Password:</label>
+        </div>
+        <div class="col-75">
+        <input type="password" placeholder="Enter Password" name="psw" id="psw" required > 
+        <span class="error">* <?php echo $pswErr;?></span>
+      </div>
+    </div>    
+    <div class="row">
+        <div class="col-25">
+            <label for="psw-repeat">Re-Enter Your Password: </label>
+        </div>
+        <div class="col-75">
+            <input type="password" placeholder="Re-Enter Your Password" name="psw_repeat" id="psw_repeat" required>
+            <span class="error">* <?php echo $psw_repeatErr;?></span>
+      </div>
+    </div>    
+    <div class="row">
+        <div class="col-75">
+          <center>  <br><input type="file" name="file" multiple></center>
+        </div>
+
+    </div>  
+    <div class="row">
+    <hr><center>
+            <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+      <input type="submit" value="Submit"></center>
+    </div>
+  </form>
+</div>
+</body>
