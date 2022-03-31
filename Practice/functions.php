6dc2747b8f85324830df1d6fe9d0063c02e6ef81
Jay <jayjogi444@gmail.com>
2022-03-31 13:44:56 +0530

    Fucntion done

---
+++ Practice/functions.php
@@ -0,0 +1,24 @@
+<?php
+include 'need.php';
+echo"<h1>Functions</h1>";
+
+Function sum($a,$b)
+{
+  echo  $a+$b;
+
+}
+
+
+sum(5,10);
+
+Function sum1($a,$b)
+{
+  return $a+$b;
+
+}
+space();
+echo sum(1,2);
+
+
+
+?>
---
+++ Practice/need.php
@@ -0,0 +1,5 @@
+<?php
+function space(){
+    echo"<br>";
+}
+?>
