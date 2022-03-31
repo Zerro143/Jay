7a08cb2b586d783f61638f75fa880c1affda1053
Jay <jayjogi444@gmail.com>
2022-03-31 15:46:08 +0530

    loop till 100 and store odd and even number difrently in array

---
+++ Practice/task1.php
@@ -0,0 +1,31 @@
+<?php
+include 'need.php';
+$odd = array();
+$even= array();
+for ($a = 0; $a < 100; $a++) {
+
+    if ($a % 2 == 0) {
+        $even[]=$a;
+      continue;
+    }
+    $odd[]=$a;
+}  
+
+ /*for ($b = 0; $b < 100; $b++) {
+    if ($b % 2 != 0) {
+      continue;
+    }
+    $even[]=$b;
+}*/
+echo count($odd)."<br>";
+echo count($even);
+space();
+$arrlength = count($odd);
+
+for($x = 0; $x < $arrlength; $x++) {
+    echo "This is Odd Number ".$odd[$x];
+    echo " This is Even Number ".$even[$x];
+    space();
+}
+
+?>
