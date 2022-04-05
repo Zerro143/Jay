87d39c424c04a633cb22f9dc73aba4a5c18e85d6
Jay <jayjogi444@gmail.com>
2022-04-05 12:32:57 +0530

    file handling Skip as not able to write in the file

--- Practice/filehandling/readfile.php
+++ Practice/filehandling/readfile.php
@@ -10,4 +10,16 @@ $myfile = fopen("dict.txt", "r") or die("Unable to open file!");
 ln();
 echo fread($myfile,filesize("dict.txt"));
 fclose($myfile);
+space();
+ln();
+$myfile = fopen("nw.txt", "a") or die("Unable to open file!");
+$txt = "Donald Duck\n";
+fwrite($myfile, $txt);
+$txt = "Goofy Goof\n";
+fwrite($myfile, $txt);
+echo fread($myfile,filesize("nw.txt"));
+fclose($myfile);
+$myfile = fopen("nw.txt", "a+");
+fwrite($myfile, $txt);
+echo fread($myfile,filesize("nw.txt"));
 ?>
