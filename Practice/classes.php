e3ab6b60839454cc08f8b805883b47f3a143be8b
Jay <jayjogi444@gmail.com>
2022-04-06 12:00:22 +0530

    Uptil destructor done

--- Practice/classes.php
+++ Practice/classes.php
@@ -26,8 +26,29 @@ $bambam = new pet();
 $bambam -> name = "Bambam"; $bambam ->  owner = "" ; $bambam-> breed = "Cat";
 echo "Pet name: ".$bambam->name."<br>  Pets Owner: " .$bambam-> owner."<br> Pets Breed: ".$bambam->breed;
 
+class emp{
+    #Properties
+    public $name;
+     public $vehical;
+    
+    #method
+    function __construct($name,$vehical)
+    {
+        $this-> name = $name;
+        $this -> vh = $vehical;
+    }        
+    function __destruct() {
+        echo "The Employee {$this->name} has Vehical {$this -> vh}.<br>"; 
+      }
+}
 
-echo $name ;
-
+$jay = new emp("Jay","Pulsar 135");
+$ritesh = new emp("Ritesh","Apache 160 4v");
+space();
+echo "Employee: ".$jay-> name. " has Vehical:".$jay -> vh;
+space();
+echo "Employee: ".$ritesh-> name. " has Vehical:".$ritesh -> vh;
+space();
+ln();
 
 ?>
