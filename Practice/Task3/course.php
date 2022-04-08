7fdf28f2aba7c92f784f43a9bfd965c033dcfe68
Jay <jayjogi444@gmail.com>
2022-04-08 19:49:10 +0530

    asthatic changes

--- Practice/Task3/course.php
+++ Practice/Task3/course.php
@@ -62,7 +62,7 @@ function closeForm() {
 <html>
     <head>
         <title>Student Grid</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     </head>
 
