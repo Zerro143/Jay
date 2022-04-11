c461605c3c98d65a15eba195a50c25719a4003ba
Jay <jayjogi444@gmail.com>
2022-04-11 15:16:25 +0530

    Just added the duplicay check

--- Practice/Task5/curl.php
+++ Practice/Task5/curl.php
@@ -26,14 +26,16 @@ for($i=0; $i<=count($data['entries']); $i++){
     $link = $data['entries'][$i]['Link'];
     $cat = $data['entries'][$i]['Category'];
 
-    $r1 = "SELECT * FROM api  data WHERE linl = '$link';";
-    mysqli_query($conn,$query);
-    $count = mysqli_num_rows($r1);
+    $r1 = "SELECT COUNT(*) FROM api  data WHERE linl = '$link';";
+    $a = mysqli_query($conn,$query);
+    if($a==0){
+        $query="INSERT INTO apidata VALUES('$api','$des','$auth','$https','$cors','$link','$cat')";
+        mysqli_query($conn,$query);
 
-    $query="INSERT INTO apidata VALUES('$api','$des','$auth','$https','$cors','$link','$cat')";
-    mysqli_query($conn,$query);
+    }    
+
+    
     //echo "$api <br> $des <br> $auth <br> $https<br> $cors <br> $link <br> $cat";
     //echo $query."<br>";
-
 }
 ?>  
