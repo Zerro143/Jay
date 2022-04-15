8a8c504cb016ba7f32a1b69bfcf49af66c0936c8
Jay <jayjogi444@gmail.com>
2022-04-15 18:10:06 +0530

    completed conditionals

---
+++ Practice/JS/conditionals.js
@@ -0,0 +1,10 @@
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("wish").innerHTML = greeting;
