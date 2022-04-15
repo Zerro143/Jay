aaeca9b4dfcb98e73b6a3ee28a8208740bf88829
Jay <jayjogi444@gmail.com>
2022-04-15 17:38:48 +0530

    Added mostly all maths functions

--- Practice/JS/math.js
+++ Practice/JS/math.js
@@ -6,4 +6,19 @@ document.getElementById("demo").innerHTML =
 "<p><b>Math.LN2:</b> " + Math.LN2 + "</p>" +
 "<p><b>Math.LN10:</b> " + Math.LN10 + "</p>" +
 "<p><b>Math.LOG2E:</b> " + Math.LOG2E + "</p>" +
-"<p><b>Math.Log10E:</b> " + Math.LOG10E + "</p>";
+"<p><b>Math.Log10E:</b> " + Math.LOG10E + "</p>"+
+"<p>"+ Math.round(4.6) + 
+"<p>"+ Math.round(4.5) +
+"<p>"+ Math.round(4.4) +
+"<p>"+ Math.ceil(-4.2) + 
+"<p>"+ Math.floor(4.2) + 
+"<p>"+ Math.trunc(-4.2)+ 
+"<p>"+ Math.sign(-4)   +
+"<p>"+ Math.sqrt(64)   +
+"<p>"+ Math.sqrt(64)   +
+"<p>"+ Math.sin(90 * Math.PI / 180) +
+"<p>"+ Math.cos(0 * Math.PI / 180)  +
+"<p>"+ Math.min(0, 150, 30, 20, -8, -200) + 
+"<p>"+ Math.max(0, 150, 30, 20, -8, -200) +
+"<p>"+ Math.random()+ 
+"<p>"+ Math.log(3);
