243cbc4b36f8b7bd2b92ee19321c8602c711da65
Jay <jayjogi444@gmail.com>
2022-04-15 17:46:34 +0530

    Some Formating for maths functions

--- Practice/JS/math.js
+++ Practice/JS/math.js
@@ -1,4 +1,4 @@
-document.getElementById("demo").innerHTML = 
+document.getElementById("maths").innerHTML = 
 "<p><b>Math.E:</b> " + Math.E + "</p>" +
 "<p><b>Math.PI:</b> " + Math.PI + "</p>" +
 "<p><b>Math.SQRT2:</b> " + Math.SQRT2 + "</p>" +
@@ -7,18 +7,17 @@ document.getElementById("demo").innerHTML =
 "<p><b>Math.LN10:</b> " + Math.LN10 + "</p>" +
 "<p><b>Math.LOG2E:</b> " + Math.LOG2E + "</p>" +
 "<p><b>Math.Log10E:</b> " + Math.LOG10E + "</p>"+
-"<p>"+ Math.round(4.6) + 
-"<p>"+ Math.round(4.5) +
-"<p>"+ Math.round(4.4) +
-"<p>"+ Math.ceil(-4.2) + 
-"<p>"+ Math.floor(4.2) + 
-"<p>"+ Math.trunc(-4.2)+ 
-"<p>"+ Math.sign(-4)   +
-"<p>"+ Math.sqrt(64)   +
-"<p>"+ Math.sqrt(64)   +
-"<p>"+ Math.sin(90 * Math.PI / 180) +
-"<p>"+ Math.cos(0 * Math.PI / 180)  +
-"<p>"+ Math.min(0, 150, 30, 20, -8, -200) + 
-"<p>"+ Math.max(0, 150, 30, 20, -8, -200) +
-"<p>"+ Math.random()+ 
-"<p>"+ Math.log(3);
+"<p><b>Math.round(4.6): </b> "+ Math.round(4.6) + 
+"<p><b>Math.round(4.5): </b> "+ Math.round(4.5) +
+"<p><b>Math.round(4.4): </b> "+ Math.round(4.4) +
+"<p><b>Math.ceil(-4.2):</b> "+ Math.ceil(-4.2) + 
+"<p><b>Math.floor(4.2):</b> "+ Math.floor(4.2) + 
+"<p><b>Math.trunc(-4.2):</b> "+ Math.trunc(-4.2)+ 
+"<p><b>Math.sign(-4):</b> "+ Math.sign(-4)   +
+"<p><b>Math.sqrt(64):</b> "+ Math.sqrt(64)   +
+"<p><b>Math.sin(90 * Math.PI / 180):</b> "+ Math.sin(90 * Math.PI / 180) +
+"<p><b>Math.cos(0 * Math.PI / 180):</b> "+ Math.cos(0 * Math.PI / 180)  +
+"<p><b>Math.min(0, 150, 30, 20, -8, -200):</b> "+ Math.min(0, 150, 30, 20, -8, -200) + 
+"<p><b>Math.max(0, 150, 30, 20, -8, -200):</b> "+ Math.max(0, 150, 30, 20, -8, -200) +
+"<p><b>Math.random():</b> "+ Math.random()+ 
+"<p><b>Math.log(3):</b> "+ Math.log(3);
