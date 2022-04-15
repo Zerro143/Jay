080c098d2aea04448dcdc3c75295f8653cb1daaa
Jay <jayjogi444@gmail.com>
2022-04-15 18:13:25 +0530

    well added the show.html to view js actions

--- Practice/JS/conditionals.js
+++ Practice/JS/conditionals.js
@@ -3,7 +3,7 @@ let greeting;
 if (time < 10) {
   greeting = "Good morning";
 } else if (time < 20) {
-  greeting = "Good day";
+  greeting = "Good Afternoon";
 } else {
   greeting = "Good evening";
 }
