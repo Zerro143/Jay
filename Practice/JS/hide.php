67c6a733e8c7c9fe8e5106eb7560e47432aacbf5
Jay <jayjogi444@gmail.com>
2022-04-25 10:29:33 +0530

    intro js revision done

---
+++ Practice/JS/hide.php
@@ -0,0 +1,8 @@
+<html>
+   <body>
+      <h2>What Can JavaScript Do?</h2>
+      <p id="demo">JavaScript can hide HTML elements.</p>
+      <button type="button" onclick="document.getElementById('demo').style.display='none'">Hide!</button>
+      <button type="button" onclick="document.getElementById('demo').style.display='block'">Reveal!</button>
+   </body>
+</html>
