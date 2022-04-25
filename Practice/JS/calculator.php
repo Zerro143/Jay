78da3f7026e63f55d9a9eb7364ed0b46f3d507ed
Jay <jayjogi444@gmail.com>
2022-04-25 16:52:37 +0530

    saprated the js and html

--- Practice/JS/calculator.php
+++ Practice/JS/calculator.php
@@ -1,26 +1,6 @@
 <html>
    <head>
-      <script>
-         //function that display value
-         function dis(val)
-         {
-         	document.getElementById("result").value+=val
-         }
-         
-         //function that evaluates the digit and return result
-         function solve()
-         {
-         	let x = document.getElementById("result").value
-         	let y = eval(x)
-         	document.getElementById("result").value = y
-         }
-         
-         //function that clear the display
-         function clr()
-         {
-         	document.getElementById("result").value = ""
-         }
-      </script>
+   <script src="calc.js"></script>
       <!-- for styling -->
       <style>
         
