b0d2a4ccaf434ff14041ea1e0fd8c2a71a1e2ae6
Jay <jayjogi444@gmail.com>
2022-04-27 19:35:21 +0530

    done changes

--- Practice/PHP/caj/connection.php
+++ Practice/PHP/caj/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "Admin@123";
-$db="blog_samples";
+$db="jay";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
