e7925bebb904e5d852a46bc18a41e91f15e95d3e
Jay <jayjogi444@gmail.com>
2022-06-13 16:07:50 +0530

    read data from the csv file and then shown output in table

--- Practice/PHP/filehandling/compare.php
+++ Practice/PHP/filehandling/compare.php
@@ -1,20 +1,27 @@
 <?php 
 include '../need.php';
+$csv = fopen("output.csv","r");
+echo "<table>";
+while (($getData = fgetcsv($csv, 10000, ",")) !== FALSE){
+    $fname = $getData[0];
+    $lname = $getData[1];
+    $email = $getData[2];
+    $m = $getData[3];
+    $course = $getData[4];
+    $bdate = $getData[5];
+    $cdate = $getData[6];
+    $udate = $getData[7];
 
+    echo "<tr><td>".$fname."</td>";
+    echo "<td>".$lname."</td>";
+    echo "<td>".$email."</td>";
+    echo "<td>".$m."</td>";
+    echo "<td>".$course."</td>";
+    echo "<td>".$bdate."</td>";
+    echo "<td>".$cdate."</td>";
+    echo "<td>".$udate."</td></tr>";
+   
+}
+echo "</table>";
+fclose($csv)
 ?>
-<form action="compare.php" method="post" enctype="multipart/form-data" id="f">
-    <div class="form-group">
-        <label for="exampleFormControlFile1">Please Select File</label>
-        <input type="file" name="file" class="form-control-file" id="exampleFormControlFile1">
-    </div>
-    <div class="form-group">
-        <input type="submit" id = "btn"name="submit" value="submit" class="btn btn-primary">
-    </div>
-</form>
-<script src="assets/js/jquery.js"></script>
-<script>
-    $("#btn").click(function(e){
-        e.preventDefault();
-        
-    })
-</script>
