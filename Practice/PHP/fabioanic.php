d3e84fff355affa48f40dc93e03661cb212d21bd
Jay <jayjogi444@gmail.com>
2022-06-13 15:26:49 +0530

    Fibonacci  Completed

---
+++ Practice/PHP/fabioanic.php
@@ -0,0 +1,14 @@
+<?php
+echo "Printing Fabioanic series <br>";
+
+$num = 10;$n1 = 0; $n2 = 1;
+echo "<table>";
+
+for($i=0; $i<$num; $i++){
+    echo "<tr><td>".$n1."</td></tr>";
+   $nth = $n1 + $n2;
+   $n1 = $n2;
+   $n2 = $nth;
+}
+echo "</table>";
+?>
