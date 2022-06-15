f4db2eea50ccd0cd76abb923cb20aa37945441a2
Jay <jayjogi444@gmail.com>
2022-06-15 12:54:03 +0530

    added in model to get data from database

---
+++ Practice/Tasks/Curd(mvc)/model/course.php
@@ -0,0 +1,12 @@
+<?php 
+class course{
+    //table fields
+    public $course_id;
+    public $course;
+
+    function __construct()
+    {
+        $course_id=0;$course="";
+    }
+}
+?>
---
+++ Practice/Tasks/Curd(mvc)/model/courseModel.php
@@ -0,0 +1,49 @@
+<?php
+class courseModel{
+    //ser database config for Mysql
+    function __construct($consetup)
+    {
+        $this -> host = $consetup -> host;
+        $this -> user = $consetup -> user;
+        $this -> pass = $consetup -> pass;
+        $this -> db = $consetup -> db;
+    }
+
+    //open mysql database
+    public function open_db(){
+
+        $this->condb = new mysqli($this->host,$this->user,$this->pass,$this->db);
+        if ($this->condb->connect_error){
+            die("Error in Connection: ".$this->condb->connect_error);
+        }
+    }
+    
+    public function close_db(){
+        $this->condb->close();
+    }
+
+    public function selectRecord($course_id){
+        try{
+            $this->open_db();
+            // if($course_id>0){
+                // $query=$this->condb->prepare("SELECT * From course WHERE course_id=?");
+                // $query->bind_param("i",$course_id);
+            // }
+            //else
+            $query=$this->condb->prepare("SELECT * FROM course");
+
+            $query->execute();
+            $res=$query->get_result();
+            $query->close();
+            $this->close_db();
+            return $res;
+        }
+        catch(Exception $e){
+            $this->close_db();
+            throw $e;
+        }
+    }
+
+
+}
+?>
