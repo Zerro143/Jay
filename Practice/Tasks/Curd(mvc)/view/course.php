dcf15124283d6be057d2742738158b894ddb3d3a
Jay <jayjogi444@gmail.com>
2022-06-27 11:28:10 +0530

    course model title centered

--- Practice/Tasks/Curd(mvc)/view/course.php
+++ Practice/Tasks/Curd(mvc)/view/course.php
@@ -46,7 +46,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Course</h4>
+                    <h4 class="modal-title text-center"><b>Course</b></h4>
                 </div>
                 <div class="modal-body">
                    
