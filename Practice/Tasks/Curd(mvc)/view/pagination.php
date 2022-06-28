69f28ab39920ccfaac75087f6a164ed651fc049d
Jay <jayjogi444@gmail.com>
2022-06-28 10:42:01 +0530

    data show

--- Practice/Tasks/Curd(mvc)/view/pagination.php
+++ Practice/Tasks/Curd(mvc)/view/pagination.php
@@ -1,10 +1,8 @@
 <center>
-    <div class="container justify-content-center"></div>
-         
-            
-                <div class ="pagination" id="page">
-
-                </div>
+  <div class="container "></div>
+    <div class ="pagination" id="page">
 
     </div>
+
+  </div>
 </center>
