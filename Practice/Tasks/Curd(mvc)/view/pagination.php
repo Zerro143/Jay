03bbeaf4373d7292ed6f9532493919a98f6fcfe2
Jay <jayjogi444@gmail.com>
2022-06-28 12:29:47 +0530

    addded bit of css

--- Practice/Tasks/Curd(mvc)/view/pagination.php
+++ Practice/Tasks/Curd(mvc)/view/pagination.php
@@ -1,3 +1,13 @@
+<style>
+  #page{
+    float: center;
+    /* padding-left: 10%; */
+  }
+  #entries{
+    float: left;
+    padding-left: 20%;
+  }
+</style>
 <center>
   <div class="container "></div>
     <div class="pagination" id="entries"></div>
