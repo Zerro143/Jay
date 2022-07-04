5dd191ab42bf347511032a8e24e75814fd3f8b6e
Jay <jayjogi444@gmail.com>
2022-07-04 17:41:46 +0530

    extra code removed

--- Practice/Tasks/Curd(mvc)/assets/js/craction.js
+++ Practice/Tasks/Curd(mvc)/assets/js/craction.js
@@ -1,88 +1,3 @@
-// function sortTable(n) {
-//     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-//     table = document.getElementById("tda");
-//     switching = true;
-//     //Set the sorting direction to ascending:
-//     dir = "asc";
-//     /*Make a loop that will continue until
-//     no switching has been done:*/
-//     while (switching) {
-//         //start by saying: no switching is done:
-//         switching = false;
-//         rows = table.rows;
-//         /*Loop through all table rows (except the
-//         first, which contains table headers):*/
-//         for (i = 1; i < (rows.length - 1); i++) {
-//             //start by saying there should be no switching:
-//             shouldSwitch = false;
-//             /*Get the two elements you want to compare,
-//             one from current row and one from the next:*/
-//             x = rows[i].getElementsByTagName("td")[n];
-//             y = rows[i + 1].getElementsByTagName("td")[n];
-//             /*check if the two rows should switch place,
-//             based on the direction, asc or desc:*/
-//             if (dir == "asc") {
-//                 if (x.innerHTML.match(/^-?\d+$/) && y.innerHTML.match(/^-?\d+$/)) {
-//                     if (Number(x.innerHTML) > Number(y.innerHTML)) {
-//                         //if so, mark as a switch and break the loop:
-//                         shouldSwitch = true;
-//                         break;
-//                     }
-//                 } else if (x.innerHTML.match(/^\d+\.\d+$/) && y.innerHTML.match(/^\d+\.\d+$/)) {
-//                     if (Number(x.innerHTML) > Number(y.innerHTML)) {
-//                         //if so, mark as a switch and break the loop:
-//                         shouldSwitch = true;
-//                         break;
-//                     }
-//                 } else {
-//                     if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-//                         //if so, mark as a switch and break the loop:
-//                         shouldSwitch = true;
-//                         break;
-//                     }
-//                 }
-//             } else if (dir == "desc") {
-//                 if (x.innerHTML.match(/^-?\d+$/) && y.innerHTML.match(/^-?\d+$/)) {
-//                     if (Number(x.innerHTML) < Number(y.innerHTML)) {
-//                         //if so, mark as a switch and break the loop:
-//                         shouldSwitch = true;
-//                         break;
-//                     }
-//                 } else if (x.innerHTML.match(/^\d+\.\d+$/) && y.innerHTML.match(/^\d+\.\d+$/)) {
-//                     if (Number(x.innerHTML) < Number(y.innerHTML)) {
-//                         //if so, mark as a switch and break the loop:
-//                         shouldSwitch = true;
-//                         break;
-//                     }
-//                 } else {
-//                     if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-//                         //if so, mark as a switch and break the loop:
-//                         shouldSwitch = true;
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
-//         if (shouldSwitch) {
-//             /*If a switch has been marked, make the switch
-//             and mark that a switch has been done:*/
-//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//             switching = true;
-//             //Each time a switch is done, increase this count by 1:
-//             switchcount++;
-//         } else {
-//             /*If no switching has been done AND the direction is "asc",
-//             set the direction to "desc" and run the while loop again.*/
-//             if (switchcount == 0 && dir == "asc") {
-//                 dir = "desc";
-//                 switching = true;
-//             }
-//         }
-//     }
-// }
-
-
-
 function del_sel(){
     var allVals = [];  
     $(".sil:checked").each(function(){
@@ -261,7 +176,7 @@ $(document).ready(function(){
         }
         da();
     });
-    
+
     $("#cr_name").on("click",function(e){
         e.preventDefault();
         var order = "";
